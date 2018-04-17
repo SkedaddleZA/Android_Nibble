c0189f7bf527c7e648b475dd64da5c3c1f7be8b1
Christopher Browne <chrismb2gun@gmail.com>
2018-04-17 18:20:09 +0200

    Made Changes to GUI -> underlined text boxes (whole app)

--- app/src/main/java/com/nibble/skedaddle/nibble/RegisterActivity.java
+++ app/src/main/java/com/nibble/skedaddle/nibble/RegisterActivity.java
@@ -29,13 +29,22 @@ public class RegisterActivity extends AppCompatActivity {
 
     //Declare all variables
     EditText etName,etSurname,etEmail,etPhone,etPassword,etCPassword;
-    Button bRegister;
+    Button bRegister, bNext1, bNext2;
     RequestQueue requestQueue;
 
     ProgressBar pb_Register;
     TextView tvLogin;
 
+    //Dialog function is used many times so creating a method decreased amount of code
+    public void messageshow(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+        builder.setMessage(message)
+                .setNegativeButton("Retry", null)
+                .create()
+                .show();
 
+        pb_Register.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +66,21 @@ public class RegisterActivity extends AppCompatActivity {
         bRegister =  findViewById(R.id.bRegister);
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         pb_Register = findViewById(R.id.pb_Register);
+        bNext1 = findViewById(R.id.bNext1);
+        bNext2 = findViewById(R.id.bNext2);
+
+        //Making only 1step buttons and textboxes available
+        bNext2.setVisibility(View.INVISIBLE);
+        bRegister.setVisibility(View.INVISIBLE);
+        bNext1.setVisibility(View.VISIBLE);
+
+        etName.setVisibility(View.VISIBLE);
+        etSurname.setVisibility(View.VISIBLE);
+        etEmail.setVisibility(View.INVISIBLE);
+        etPhone.setVisibility(View.INVISIBLE);
+        etPassword.setVisibility(View.INVISIBLE);
+        etCPassword.setVisibility(View.INVISIBLE);
+
 
         //Button to Change screen to Login screen
         tvLogin.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +94,52 @@ public class RegisterActivity extends AppCompatActivity {
 
         CommonMethods.buttonEffect(bRegister);//Button Press effect implementation
 
+
+        bNext1.setOnClickListener(new View.OnClickListener() {//change screen layout from 1st step to 2nd step
+            @Override
+            public void onClick(View v) {
+
+                if(etName.getText().toString().matches("")) {
+                    messageshow("Please enter name.");
+                }else if(etSurname.getText().toString().matches("")) {
+                    messageshow("Please enter surname.");
+                }else {
+
+                    etName.setVisibility(View.INVISIBLE);
+                    etSurname.setVisibility(View.INVISIBLE);
+                    etEmail.setVisibility(View.VISIBLE);
+                    etPhone.setVisibility(View.VISIBLE);
+                    etPassword.setVisibility(View.INVISIBLE);
+                    etCPassword.setVisibility(View.INVISIBLE);
+                    bNext1.setVisibility(View.INVISIBLE);
+                    bNext2.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        bNext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(etEmail.getText().toString().matches(""))  {
+                    messageshow("Please enter email.");
+                }  else if(etPhone.getText().toString().matches(""))  {
+                    messageshow("Please enter phone.");
+                }else {
+                    etName.setVisibility(View.INVISIBLE);
+                    etSurname.setVisibility(View.INVISIBLE);
+                    etEmail.setVisibility(View.INVISIBLE);
+                    etPhone.setVisibility(View.INVISIBLE);
+                    etPassword.setVisibility(View.VISIBLE);
+                    etCPassword.setVisibility(View.VISIBLE);
+                    bNext2.setVisibility(View.INVISIBLE);
+                    bRegister.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+
         //Button Register click event
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,16 +151,9 @@ public class RegisterActivity extends AppCompatActivity {
                 String phone = etPhone.getText().toString();
                 String password = etPassword.getText().toString();
                 String cpassword = etCPassword.getText().toString();
+
                 //Simple input not null/empty check
-                if(name.matches("")) {
-                    messageshow("Please enter name.");
-                }else if(surname.matches("")) {
-                    messageshow("Please enter surname.");
-                }else if(email.matches(""))  {
-                    messageshow("Please enter email.");
-                }  else if(phone.matches(""))  {
-                    messageshow("Please enter phone.");
-                }else if(password.matches(""))  {
+                if(password.matches(""))  {
                     messageshow("Please enter password.");
                 }else if(!password.matches(cpassword)){
                     messageshow("Password mismatch");
@@ -129,16 +192,7 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
 
-            //Dialog function is used many times so creating a method decreased amount of code
-            public void messageshow(String message){
-                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                builder.setMessage(message)
-                        .setNegativeButton("Retry", null)
-                        .create()
-                        .show();
 
-                pb_Register.setVisibility(View.INVISIBLE);
-            }
         });
 
     }
