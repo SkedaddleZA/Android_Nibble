641946615928612b9947e511b2e144b06620865e
marno <marnogouws@gmail.com>
2018-06-01 12:40:39 +0200

    Added My Booking Screen

---
+++ app/src/main/java/com/nibble/skedaddle/nibble/activities/MyBookingActivity.java
@@ -0,0 +1,8 @@
+package com.nibble.skedaddle.nibble.activities;
+
+/**
+ * Created by marno on 2018/05/26.
+ */
+
+public class MyBookingActivity {
+}
--- app/src/main/java/com/nibble/skedaddle/nibble/classes/DAL.java
+++ app/src/main/java/com/nibble/skedaddle/nibble/classes/DAL.java
@@ -32,6 +32,7 @@ class DAL {
     private static final String restaurantsByTypeUrl ="http://SICT-IIS.nmmu.ac.za/skedaddle/getRestaurantsByType.php";
     private static final String requestBookingUrl = "http://SICT-IIS.nmmu.ac.za/skedaddle/bookingRequest.php";
     private static final String restaurantDetails ="http://SICT-IIS.nmmu.ac.za/skedaddle/getRestaurantDetails.php";
+    private static final String myBookingrequest ="http://SICT-IIS.nmmu.ac.za/skedaddle/getMyBookingRequest.php";
 
    //Insert customer procedure
     public void InsertCustomer(final String FirstName, final String LastName, final String Email, final String Phone, final String Password, Response.Listener<String> listener, RequestQueue requestQueue)
@@ -125,6 +126,26 @@ class DAL {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> parameters = new HashMap<String, String>();
                 parameters.put("restaurantid", RestaurantID);
+
+                return parameters;
+
+            }
+
+        };
+        requestQueue.add(request);
+    }
+    public void GetMyBookingRequest(final String CustomerID, Response.Listener<String> listener, RequestQueue requestQueue)
+    {
+        StringRequest request = new StringRequest(Request.Method.POST, myBookingrequest, listener ,new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("customerID", CustomerID);
 
                 return parameters;
 
