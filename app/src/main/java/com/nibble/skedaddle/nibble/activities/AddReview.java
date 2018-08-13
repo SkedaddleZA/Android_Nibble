b70119623c7aba6d5b88da9eeadc666e9f7a8e64
Christopher Browne <chrismb2gun@gmail.com>
2018-08-13 06:58:55 +0200

    Added Activities for reviews

---
+++ app/src/main/java/com/nibble/skedaddle/nibble/activities/AddReview.java
@@ -0,0 +1,15 @@
+package com.nibble.skedaddle.nibble.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nibble.skedaddle.nibble.R;
+
+public class AddReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+    }
+}
---
+++ app/src/main/java/com/nibble/skedaddle/nibble/activities/ViewReviews.java
@@ -0,0 +1,15 @@
+package com.nibble.skedaddle.nibble.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nibble.skedaddle.nibble.R;
+
+public class ViewReviews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reviews);
+    }
+}
